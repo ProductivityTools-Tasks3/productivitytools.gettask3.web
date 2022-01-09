57f264138df971361a2b5839d7b4d564925e286b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-01-09 12:03:14 +0100

    item details

--- src/Components/Console/index.js
+++ src/Components/Console/index.js
@@ -9,7 +9,7 @@ import ItemDetails from '../ItemDetails';
 export default function Console() {
 
     const [list, setList] = useState([]);
-    const [selectedNode, setSelectedNode] = useState(-1);
+    const [selectedElement, setSelectedElement] = useState();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,21 +23,16 @@ export default function Console() {
     }, []);
 
     function findElement(candidateElement, nodeId) {
-        //debugger;
-        console.log("findelement");
         var candidateElementId = candidateElement.elementId.toString();
         console.log(candidateElement.elementId);
         console.log(candidateElementId);
         if (candidateElementId == nodeId) {
-            console.log("firstelement");
             return candidateElement;
         } else {
             for (var i = 0; i < candidateElement.elements.length; i += 1) {
-                console.log("for invoked");
                 var newCandidateElement = candidateElement.elements[i];
                 var result = findElement(newCandidateElement, nodeId);
                 if (result != null) {
-                    console.log("find result");
                     return result;
                 }
             }
@@ -46,18 +41,16 @@ export default function Console() {
 
     function nodeSelect(nodeId) {
         console.log(list);
-        var selectedElement = findElement(list, nodeId);
-        console.log("selectedelement")
-        console.log(selectedElement);
-        setSelectedNode(nodeId)
+        var se = findElement(list, nodeId);
+        setSelectedElement(se);
     }
 
     return (
         <div>
             <p>Console</p>
             <StructureTree list={list} nodeSelect={nodeSelect} />
-            <ItemDetails />
-            <p>{selectedNode}</p>
+            <ItemDetails selectedElement={selectedElement} />
+            <p>{selectedElement && selectedElement.elementId}</p>
         </div>
     )
 }
--- src/Components/ItemDetails/index.js
+++ src/Components/ItemDetails/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function ItemDetails(){
-    return (<div>Item details</div>)
+export default function ItemDetails(props){
+    return (<div>Item details
+        <p><span>ElementId: </span><span>{props.selectedElement.elementId}</span></p>
+        <p><span>Name: </span><span>{props.selectedElement.name}</span></p>
+        <p><span>Status: </span><span>{props.selectedElement.status}</span></p>
+        <p><span>Created: </span><span>{props.selectedElement.created}</span></p>
+        <p><span>Started: </span><span>{props.selectedElement.started}</span></p>
+        <p><span>Finished: </span><span>{props.selectedElement.finished}</span></p>
+        <p><span>Amout of child elements: </span><span>{props.selectedElement.elements.length}</span></p>
+
+    </div>)
 }
