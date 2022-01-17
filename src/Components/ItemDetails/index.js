37418798100d3fff6db2a0a28877607bd4424d01
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-01-17 06:40:13 +0100

    format

--- src/Components/ItemDetails/index.js
+++ src/Components/ItemDetails/index.js
@@ -15,7 +15,7 @@ export default function ItemDetails(props) {
     }
 
     if (props.selectedElement!=null) {
-        return (<div className='itemDetails'>Item details
+        return (<div className='itemDetails'>
             <p><span>ElementId: </span><span>{props.selectedElement.elementId}</span></p>
             <p><span>Name: </span><input type="text" name="name" value={props.selectedElement.name} onChange={handleChange} ></input></p>
             <p><span>Status: </span><span>{props.selectedElement.status}</span></p>
@@ -23,6 +23,10 @@ export default function ItemDetails(props) {
             <p><span>Started: </span><span>{props.selectedElement.started}</span></p>
             <p><span>Finished: </span><span>{props.selectedElement.finished}</span></p>
             <p><span>Amout of child elements: </span><span>{props.selectedElement.elements.length}</span></p>
+            
+            
+            
+            
             <p><Button onClick={props.finishAction}>Finish</Button></p>
             <p><Button onClick={props.unDoneAction}>Undone</Button></p>
         </div>)
