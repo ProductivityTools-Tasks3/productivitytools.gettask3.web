0148a4531975f3ff22b095f4114234e75c4f7dee
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-01-17 06:54:16 +0100

    moment

--- src/Components/ItemDetails/index.js
+++ src/Components/ItemDetails/index.js
@@ -1,8 +1,10 @@
 import { Button } from "@material-ui/core";
 import React from "react";
-import apiService from "../../services/apiService";
+import Moment from 'react-moment';
 
 export default function ItemDetails(props) {
+
+    const dateFormat="YYYY-MM-DD HH:MM:SS";
 
     const handleChange=(e,o)=>{
         
@@ -19,16 +21,16 @@ export default function ItemDetails(props) {
             <p><span>ElementId: </span><span>{props.selectedElement.elementId}</span></p>
             <p><span>Name: </span><input type="text" name="name" value={props.selectedElement.name} onChange={handleChange} ></input></p>
             <p><span>Status: </span><span>{props.selectedElement.status}</span></p>
-            <p><span>Created: </span><span>{props.selectedElement.created}</span></p>
-            <p><span>Started: </span><span>{props.selectedElement.started}</span></p>
-            <p><span>Finished: </span><span>{props.selectedElement.finished}</span></p>
+            <p><span>Created: </span><span><Moment format={dateFormat}>{props.selectedElement.created}</Moment></span></p>
+            <p><span>Started: </span><span><Moment format={dateFormat}>{props.selectedElement.started}</Moment></span></p>
+            <p><span>Finished: </span><span><Moment> format={dateFormat}>{props.selectedElement.finished}</Moment></span></p>
             <p><span>Amout of child elements: </span><span>{props.selectedElement.elements.length}</span></p>
             
             
             
             
-            <p><Button onClick={props.finishAction}>Finish</Button></p>
-            <p><Button onClick={props.unDoneAction}>Undone</Button></p>
+            <p><Button variant="contained" onClick={props.finishAction}>Finish</Button></p>
+            <p><Button variant="contained" onClick={props.unDoneAction}>Undone</Button></p>
         </div>)
     }
     else
