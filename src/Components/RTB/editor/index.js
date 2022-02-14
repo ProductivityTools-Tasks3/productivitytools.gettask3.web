b6756f506a0d1e68b87543725ca6206fd9ac4117
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-14 23:16:01 +0100

    more elements added

--- src/Components/RTB/editor/index.js
+++ src/Components/RTB/editor/index.js
@@ -4,6 +4,14 @@ import Field from '../molecules/field'
 import { Global, ThemeProvider } from '@emotion/react'
 import { globalStyle, theme } from '../utils/theme.config'
 import FormLinker from 'form-linker'
+import iconLibrary from '../utils/iconLIbrary'
+import Translator from 'simple-translator'
+import english from '../utils/translations/en'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+
+
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 
 const Editor = (props) => {
@@ -22,15 +30,25 @@ const Editor = (props) => {
         }
     }))
 
-    debugger;
+    
+    iconLibrary()
+    Translator.registerDefaultLanguage("en", english)
+
     return (
         <ThemeProvider theme={theme}>
-            <Field
-                formLinker={formLinker.current}
-                name="editor"
-                type="editor"
+            <Global styles={globalStyle} />
 
-                onChange={forceUpdate} />
+            <Field
+                  formLinker={formLinker.current}
+                  name="editor"
+                  type="editor"
+                  minHeight={150}
+                  height={620}
+                  maxHeight={800}
+                  placeholder="Enter your content here"
+                  toolbar={['withImages']}
+                  onChange={forceUpdate}
+                  />
         </ThemeProvider>
     )
 }
