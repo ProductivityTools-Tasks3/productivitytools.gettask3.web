538104702c5304e50b8b4b860dbbd1f7dfaed088
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-22 06:34:10 +0100

    const change

--- src/Consts.js
+++ src/Consts.js
@@ -3,7 +3,7 @@ const dev = {
     clientId: "devtasks3web",
     PATH_BASE: 'http://localhost:5513/api/',
     //PATH_BASE: 'https://apigettask3.productivitytools.tech:8040/api/',
-    stsAuthority: 'https://identityserver.productivitytools.tech:8010/',
+    stsAuthority: 'https://localhost:5001/',
     clientRoot: 'http://localhost:3000/',
     clientScope : 'openid profile GetTask3.API'
 }
