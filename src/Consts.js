62bdfc8741ca10f0c5370e5f19aafd956a67c970
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-11-17 18:10:26 +0100

    top

--- src/Consts.js
+++ src/Consts.js
@@ -3,7 +3,7 @@ const dev = {
     clientId: "devtasks3web",
     PATH_BASE: 'http://localhost:5513/api/',
     //PATH_BASE: 'https://apigettask3.productivitytools.tech:8040/api/',
-    stsAuthority: 'https://identityserver.productivitytools.tech:8010/',
+    stsAuthority: 'https://identityserver.productivitytools.top:8010/',
     clientRoot: 'http://localhost:3000/',
     clientScope : 'openid profile GetTask3.API'
 }
@@ -11,8 +11,8 @@ const dev = {
 
 const prod = {
     clientId: "prodtasks3web",
-    PATH_BASE: 'https://apigettask3.productivitytools.tech:8040/api/',
-    stsAuthority: 'https://identityserver.productivitytools.tech:8010/',
+    PATH_BASE: 'https://apigettask3.productivitytools.top:8040/api/',
+    stsAuthority: 'https://identityserver.productivitytools.top:8010/',
     clientRoot: 'https://task3web.z13.web.core.windows.net/',
     clientScope : 'openid profile GetTask3.API'
 }
